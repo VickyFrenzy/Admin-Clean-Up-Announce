3355351b9882bcd83727ff0a2fae5582b8b771e5
VictorienXP <victorienxp@pm.me>
2023-04-08 10:33:13 +0200

    Add announce to vj_cleanup (from VJ Base)

--- lua/autorun/server/admin_clean_up_announce.lua
+++ lua/autorun/server/admin_clean_up_announce.lua
@@ -9,3 +9,19 @@ function CC_AdminCleanup(pl, command, args)
 end
 
 concommand.Add("gmod_admin_cleanup", CC_AdminCleanup, nil, "", {FCVAR_DONTRECORD})
+
+local function inject_to_vj_cleanup()
+	if not VJBASE_VERSION then return nil end
+	local CommandTable = concommand.GetTable()
+	local True_vj_cleanup = CommandTable.vj_cleanup
+	if not True_vj_cleanup then return false end
+	local function New_vj_cleanup(ply, cmd, args)
+		True_vj_cleanup(ply, cmd, args)
+		if IsValid(ply) and not ply:IsAdmin() then return end
+		PrintMessage(HUD_PRINTTALK, string.format("%s has cleaned up the map (VJ Base).", ply:Name()))
+	end
+	CommandTable.vj_cleanup = New_vj_cleanup
+	return true
+end
+
+if not inject_to_vj_cleanup() then timer.Simple(0, inject_to_vj_cleanup) end
